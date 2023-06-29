49524f6720abc50422af6a3462980fa0472e5405
written-31 <115435294+written-31@users.noreply.github.com>
2023-06-29 12:56:48 +0900

    2023/6/29

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    //ハンバーガーメニュー
+    $(".js-hamburger").on("click", function () {
+      $(this).toggleClass("open");
+      if ($(this).hasClass("open")) {
+        $(".js-sp-nav").fadeIn();
+      } else {
+        $(".js-sp-nav").fadeOut();
+      }
+    });
 
 });
