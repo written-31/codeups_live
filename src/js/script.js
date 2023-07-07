641c554f9b81cdd6b41d4310fe24bed4a933a305
written-31 <115435294+written-31@users.noreply.github.com>
2023-07-07 14:53:13 +0900

    liveコーディング⑤完成

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,23 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  //ハンバーガーメニュー
+  $(".js-hamburger").on("click", function () {
+    $(this).toggleClass("open");
+    if ($(this).hasClass("open")) {
+      $(".js-sp-nav").fadeIn();
+    } else {
+      $(".js-sp-nav").fadeOut();
+    }
+  });
 
-    //ハンバーガーメニュー
-    $(".js-hamburger").on("click", function () {
-      $(this).toggleClass("open");
-      if ($(this).hasClass("open")) {
-        $(".js-sp-nav").fadeIn();
-      } else {
-        $(".js-sp-nav").fadeOut();
-      }
-    });
-
+  // 呼び出しの基本：
+  const swiper = new Swiper("#js-works-swiper", {
+    // ここにオプション設定が入ります
+    loop: true, // 無限ループ
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+  });
 });
